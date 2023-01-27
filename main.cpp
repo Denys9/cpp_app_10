638197819e1b11bad07e5289a6e175af7f079253
Dines <lozindenis6@gmail.com>
2023-01-27 21:20:20 +0200

    №8

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <ctime>
+using namespace std;
+
+void reversed(int arr[], const int size);
+
+int main()
+{
+	cout << ":_(";
+}
