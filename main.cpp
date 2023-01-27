c29665e02257a5fbfa8584a15b2d35f10d6c443b
Dines <lozindenis6@gmail.com>
2023-01-27 21:09:24 +0200

    №2

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void cube(int s1, int s2);
+void fact(int s1, int s2);
 
 int main() {
 	int s1, s2 = 1;
 	cout << "Enter 1 size: ";
 	cin >> s1;
-	cube(s1, s2);
+	fact(s1, s2);
 }
 
-void cube(int s1, int s2)
+void fact(int s1, int s2)
 {
 	for (size_t i = 1; i < s1 + 1; i++)
 	{
@@ -18,5 +18,5 @@ void cube(int s1, int s2)
 		cout << " * " << s2;;
 	}
 	cout << endl;
-	cout << "\nFucktorial " << s1 << " = " << s2;
+	cout << "\factorial " << s1 << " = " << s2;
 }
